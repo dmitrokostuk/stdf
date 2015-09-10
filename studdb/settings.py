02bdda3eb5299deb9dc77132741319bbcc961a4e
dmitrokostjuk <dimoonoo6@gmail.com>
2015-09-10 22:43:29 +0300

    add dynamic for main page

--- studdb/settings.py
+++ studdb/settings.py
@@ -100,6 +100,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
+# https://docs.djangoproject.com/en/1.8/howto/st-files/
 
 STATIC_URL = '/static/'
--- students/views.py
+++ students/views.py
@@ -1,3 +1,4 @@
+# _*_ coding: utf-8 _*_
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
@@ -5,9 +6,29 @@ from django.http import Http404
 # Create your views here.
 
 def students_list(request):
-    return render(request,'students/students_list.html',{})
+    students = (
+        {'id': 1,
+         'first_name' : u'Дмитро',
+         'last_name': u'Костюк',
+         'ticket' : 1222,
+         'image':'img/kosmos.jpg'
+         },
+        {'id':2,
+         'first_name' : u'Василів',
+         'last_name': u'Василь',
+         'ticket' : 12,
+         'image':'img/kosmos.jpg'},
+        {'id':3,
+         'first_name' : u'Петрів',
+         'last_name': u'Петро',
+         'ticket' : 12123,
+         'image':'img/kosmos.jpg'},
+     )
+    return render(request,'students/students_list.html',{'students':students})
+
 
 def students_add(request):
+
     return HttpResponse('<h1>Student Add Form</h1>')
 
 def students_edit(request, sid):
