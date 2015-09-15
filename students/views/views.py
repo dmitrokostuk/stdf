e8cac0019bdc70992a8bf140997e48624acb55d9
dmitrokostjuk <dimoonoo6@gmail.com>
2015-09-15 20:07:58 +0300

    add views folder

--- students/views/views.py
+++ students/views/views.py
@@ -40,7 +40,9 @@ def students_delete(request, sid):
 #Viev for Groups
 
 def groups_list(request):
-    return HttpResponse('<h1>Groups Listing </h1>')
+    return render(request,'students/groups.html')
+
+    #return HttpResponse('<h1>Groups Listing </h1>')
 
 def groups_add(request):
     return HttpResponse('<h1>Groups Add Form </h1>')
